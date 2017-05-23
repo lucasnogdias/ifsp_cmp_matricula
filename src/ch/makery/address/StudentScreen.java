50af96ce9155b07a86cee4a1459762bcb1ccc331
lucasnogdias <lucas_ndias@hotmail.com>
2017-05-22 21:29:25 -0300

    Verifica horário da disciplina para não haver requisições em horários conflitantes.

--- src/ch/makery/address/StudentScreen.java
+++ src/ch/makery/address/StudentScreen.java
@@ -131,23 +131,55 @@ public class StudentScreen {
     
     private ObservableList<Disciplina> ListarDisciplinas(){
     	//TODO: Obter do BD uma lista de disciplinas sendo o ofertadas.
+    	ObservableList<Disciplina> ofertadas = this.BuscarOfertadas();
     	//TODO: Comparar esta lista com o usuario e remover todas as disciplinas que ele não pode se matricular.
-    	ObservableList<Disciplina> disponiveis = FXCollections.observableArrayList(
+    	ObservableList<Disciplina> disponiveis = ofertadas;
+    	disponiveis = this.filtrarDisciplinas(ofertadas);
+    	return disponiveis;
+    }
+    
+    private ObservableList<Disciplina> BuscarOfertadas(){
+    	//TODO: substituir essa funçao por uma busca ao banco com as disciplinas ofertadas
+    	ObservableList<Disciplina> ofertas = FXCollections.observableArrayList(
     			new Disciplina("AOO", "N", "Quarta-Feira", "19-23", "Rafael Muniz e Samuel Martins"),
     			new Disciplina("BD2", "N", "Terça-Feira", "19-23", "Everton Silva e Carlos Beluzo"),
     			new Disciplina("ED1", "N", "Sexta-Feira", "19-23", "José Américo e Samuel Martins"),
     			new Disciplina("IHC", "N", "Quinta-Feira", "19-21", "José Américo"),
     			new Disciplina("LP3", "N", "Segunda-Feira", "19-23", "Everton Silva e André Valente"),
-    			new Disciplina("MFI", "N", "Quarta-Feira", "21-23", "Cecília Pereira de Andrade")
+    			new Disciplina("MFI", "N", "Quarta-Feira", "21-23", "Cecília Pereira de Andrade"),
+    			new Disciplina("LP1", "N", "Segunda-Feira", "19-23", "Sovat"),
+    			new Disciplina("BD1", "N", "Terça-Feira", "19-23", "Zady e Beluzo"),
+    			new Disciplina("WEB", "N", "Quinta-Feira", "19-23", "Rafael"),
+    			new Disciplina("TST", "N", "Quinta-Feira", "21-23", "José")
     	);
-    	return disponiveis;
+    	return ofertas;
+    }
+    
+    private ObservableList<Disciplina> filtrarDisciplinas(ObservableList<Disciplina> oferta){
+    	return oferta;
     }
     
     public void selecionarDisciplina(){
     	Disciplina d = this.avaliableTableView.getSelectionModel().getSelectedItem();
+    	boolean canAdd = true;
     	if (d!=null){
-    		this.disciplinasRequiridas.add(d);
-    		this.disciplinasDisponiveis.remove(d);
+    		for (int i = 0; i<this.disciplinasRequiridas.size(); i++){
+    			Disciplina old = this.disciplinasRequiridas.get(i);
+    			if (d.getDia()==old.getDia()){
+    				String[] hrNew = d.getHorario().split("-");
+    				String[] hrOld = old.getHorario().split("-");
+    				if( hrNew[0].equals(hrOld[0]) || hrNew[1].equals(hrOld[1]) ){
+    					canAdd = false;
+    				}
+    			}
+    		}
+    		if (canAdd){
+    			this.disciplinasRequiridas.add(d);
+    			this.disciplinasDisponiveis.remove(d);
+    		}else{
+    			//TODO: Exibir Menssagem de Erro
+    			System.out.println("Não é Possível Requisitar esta Disciplina. Conflito de horário");
+    		}
     	}
     }
     
@@ -157,6 +189,11 @@ public class StudentScreen {
     		this.disciplinasDisponiveis.add(d);
     		this.disciplinasRequiridas.remove(d);
     	}
+    }
+    
+    public void enviarRequisicao(){
+    	//TODO: Enviar itens da lista para a tabela 'requisicoes' no backend como uma nova requisicao.
+    	System.out.println("Requisição enviada com sucesso");
     }
 
 }
