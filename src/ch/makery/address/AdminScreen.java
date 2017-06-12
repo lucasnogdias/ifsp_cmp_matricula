597a61122d2c64b4510cd7f0288a17e1f8ec1789
lucasnogdias <lucas_ndias@hotmail.com>
2017-06-12 20:52:43 -0300

    Aceitar/negar matriculas requisitadas.

--- src/ch/makery/address/AdminScreen.java
+++ src/ch/makery/address/AdminScreen.java
@@ -88,10 +88,22 @@ public class AdminScreen {
 
     @FXML
     void aprovarReq(ActionEvent event) {
+    	//TODO: send data to backend
+    	Disciplina aprovada = this.reqTableView.getSelectionModel().getSelectedItem();
+    	ObservableList<Disciplina> list = this.reqTableView.getItems();
+    	list.remove(aprovada);
+    	this.reqTableView.setItems(list);
     }
 
     @FXML
     void recusarReq(ActionEvent event) {
+    	//TODO: send data to backend
+    	Disciplina reprovada = this.reqTableView.getSelectionModel().getSelectedItem();
+    	ObservableList<Disciplina> list = this.reqTableView.getItems();
+    	list.remove(reprovada);
+    	String motivo = this.reqJustificativa.getText();
+    	this.reqTableView.setItems(list);
+    	this.reqJustificativa.setText("");
     }
     
     @FXML 
@@ -146,7 +158,7 @@ public class AdminScreen {
         assert reqTableView != null : "fx:id=\"reqTableView\" was not injected: check your FXML file 'AdminScreen.fxml'.";
         assert reqTurma != null : "fx:id=\"reqTurma\" was not injected: check your FXML file 'AdminScreen.fxml'.";
         
-        ObservableList<Requisicao> reqList = this.BuscarRequicoes();
+        ObservableList<Requisicao> reqList = this.buscarRequisicoes();
         
         listReq.setCellValueFactory(new PropertyValueFactory<Requisicao, String>("alunoProntuario"));
     	listTableView.setItems(reqList);
@@ -172,7 +184,7 @@ public class AdminScreen {
     	});
     }
     
-    private ObservableList<Requisicao> BuscarRequicoes(){
+    private ObservableList<Requisicao> buscarRequisicoes(){
     	//TODO: substituir essa funçao por uma busca ao banco com as requisicoes em aberto
     	ObservableList<Disciplina> list = FXCollections.observableArrayList(
 				new Disciplina("AOO", "N", "Quarta-Feira", "19-23", "Rafael Muniz e Samuel Martins", new String[]{"ESW"}),
@@ -185,10 +197,32 @@ public class AdminScreen {
 				new Disciplina("BD1", "N", "Terça-Feira", "19-23", "Zady e Beluzo"),
 				new Disciplina("WEB", "N", "Quinta-Feira", "19-23", "Rafael", new String[]{"LP1"}),
 				new Disciplina("TST", "N", "Quinta-Feira", "21-23", "José"));
+    	ObservableList<Disciplina> list2 = FXCollections.observableArrayList(
+				new Disciplina("AOO", "N", "Quarta-Feira", "19-23", "Rafael Muniz e Samuel Martins", new String[]{"ESW"}),
+				new Disciplina("BD2", "N", "Terça-Feira", "19-23", "Everton Silva e Carlos Beluzo", new String[]{"BD1"}),
+				new Disciplina("ED1", "N", "Sexta-Feira", "19-23", "José Américo e Samuel Martins", new String[]{"LP2", "BD1"}),
+				new Disciplina("IHC", "N", "Quinta-Feira", "19-21", "José Américo"),
+				new Disciplina("LP3", "N", "Segunda-Feira", "19-23", "Everton Silva e André Valente", new String[]{"LP2"}),
+				new Disciplina("MFI", "N", "Quarta-Feira", "21-23", "Cecília Pereira de Andrade"),
+				new Disciplina("LP1", "N", "Segunda-Feira", "19-23", "Sovat"),
+				new Disciplina("BD1", "N", "Terça-Feira", "19-23", "Zady e Beluzo"),
+				new Disciplina("WEB", "N", "Quinta-Feira", "19-23", "Rafael", new String[]{"LP1"}),
+				new Disciplina("TST", "N", "Quinta-Feira", "21-23", "José"));
+    	ObservableList<Disciplina> list3 = FXCollections.observableArrayList(
+				new Disciplina("AOO", "N", "Quarta-Feira", "19-23", "Rafael Muniz e Samuel Martins", new String[]{"ESW"}),
+				new Disciplina("BD2", "N", "Terça-Feira", "19-23", "Everton Silva e Carlos Beluzo", new String[]{"BD1"}),
+				new Disciplina("ED1", "N", "Sexta-Feira", "19-23", "José Américo e Samuel Martins", new String[]{"LP2", "BD1"}),
+				new Disciplina("IHC", "N", "Quinta-Feira", "19-21", "José Américo"),
+				new Disciplina("LP3", "N", "Segunda-Feira", "19-23", "Everton Silva e André Valente", new String[]{"LP2"}),
+				new Disciplina("MFI", "N", "Quarta-Feira", "21-23", "Cecília Pereira de Andrade"),
+				new Disciplina("LP1", "N", "Segunda-Feira", "19-23", "Sovat"),
+				new Disciplina("BD1", "N", "Terça-Feira", "19-23", "Zady e Beluzo"),
+				new Disciplina("WEB", "N", "Quinta-Feira", "19-23", "Rafael", new String[]{"LP1"}),
+				new Disciplina("TST", "N", "Quinta-Feira", "21-23", "José"));
     	ObservableList<Requisicao> reqList = FXCollections.observableArrayList(
     			new Requisicao("160000-0",  list),
-    			new Requisicao("160000-1",  list),
-    			new Requisicao("160000-2",  list),
+    			new Requisicao("160000-1",  list2),
+    			new Requisicao("160000-2",  list3),
     			new Requisicao("160000-3",  list),
     			new Requisicao("160000-4",  list));
     	return reqList;
